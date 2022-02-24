cdb3db5b5eb65f7f9d64cb5ebe5679a3e36d6069
sebasRm <71810357+sebasRm@users.noreply.github.com>
2022-02-24 12:34:03 -0500

    actualización diseño de home

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom'
+import "../css/HomeScreen.css"
 
 export const HomeScreen = () => {
   const navigate = useNavigate();
@@ -9,8 +13,69 @@ export const HomeScreen = () => {
 
   return (
     <div className="container">
-      <h1>HomeScreen</h1>
-      <button onClick={handleStation}>File Station</button>
+      <div className="row align-items-center justify-content-center center-block minh-100">
+      <div className="col-xs-6 col-sm-12  col-md-12 col-lg-12 text-center  ">
+          <h1 className="titulo">HORUS+</h1>
+      </div>
+          <div className="col-xs-12 col-sm-12  col-md-12 col-lg-2">
+          
+            
+                <div className="card text-white bg-secondary " onClick={handleStation}>
+                    <div className="card-header text-center ">
+                      <div style={{fontSize:"3em", color:'#fed463'}}>
+                         <FontAwesomeIcon   icon={faFolder}/>
+                      </div>
+                    <p>FileStation</p>
+                    </div>
+                </div>
+
+          </div>
+
+          <div className="col-xs-12 col-sm-12  col-md-12 col-lg-2">
+              <div className="card text-white bg-secondary " onClick={handleStation}>
+                    <div className="card-header text-center ">
+                      <div style={{fontSize:"3em", color:'#cbdb52'}}>
+                         <FontAwesomeIcon   icon={faFolder}/>
+                      </div>
+                    <p>Usuarios</p>
+                    </div>
+                </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-12  col-md-12 col-lg-2">
+          <div className="card text-white bg-secondary " onClick={handleStation}>
+                    <div className="card-header text-center ">
+                      <div style={{fontSize:"3em", color:'#cbdb52'}}>
+                         <FontAwesomeIcon   icon={faFolder}/>
+                      </div>
+                    <p>FileStation</p>
+                    </div>
+                </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-12  col-md-12 col-lg-2">
+          <div className="card text-white bg-secondary " onClick={handleStation}>
+                    <div className="card-header text-center ">
+                      <div style={{fontSize:"3em", color:'#fed463'}}>
+                         <FontAwesomeIcon   icon={faFolder}/>
+                      </div>
+                    <p>FileStation</p>
+                    </div>
+                </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-12  col-md-12 col-lg-2">
+          <div className="card text-white bg-secondary " onClick={handleStation}>
+                    <div className="card-header text-center ">
+                      <div style={{fontSize:"3em", color:'#fed463'}}>
+                         <FontAwesomeIcon   icon={faFolder}/>
+                      </div>
+                    <p>FileStation</p>
+                    </div>
+                </div>
+          </div>
+
+      </div>
     </div>
   );
 };
